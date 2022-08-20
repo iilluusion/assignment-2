ccc1bdf724645cd7a08f4b4a052f010301f39ae9
vaishali modi <111068799+iilluusion@users.noreply.github.com>
2022-08-20 14:08:12 +0530

    Update create the dictionary.py

--- create the dictionary.py
+++ create the dictionary.py
@@ -1,4 +1,6 @@
 x={}
-for i in range(97,123):
+n1=int(input("enter the number from where range start"))
+n2=int(input("enter the number end of range"))
+for i in range(n1,n2+1):
     x[chr(i)]=i
 print("dictionary=",x)    
