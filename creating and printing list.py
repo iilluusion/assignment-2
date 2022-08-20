2308608b57fde23fe0be4f889ca62f037010a109
vaishali modi <111068799+iilluusion@users.noreply.github.com>
2022-08-20 14:33:27 +0530

    Update creating and printing list.py

--- creating and printing list.py
+++ creating and printing list.py
@@ -1,4 +1,8 @@
-list1=[(2,5),(1,2),(4,4),(2,3),(2,1)]
+list1=[]
+n=int(input("enter number"))
+for i in range(n):
+    t=tuple(map(int,input().split())
+            list1.append(t)
 print("List of tuple before sorting : " + str(list1))
 for i in range(0,len(list1)):
     for j in range(0,len(list1) -i-1):
